c8020ad372484e97d01ac16a298ef854324d45cf
Eugene <lin.eugene.l.e@gmail.com>
2023-07-06 15:22:26 +0800

    Edit the color theme

--- sched-helper/app/components/navbar.tsx
+++ sched-helper/app/components/navbar.tsx
@@ -6,21 +6,10 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 
 export default function NavBar({children, items}:{children: React.ReactNode, items?: React.ReactNode[]}){
-    // let navItems: React.ReactNode[] = [] 
-    // if(items){
-    //   navItems = items.map((item, index) => {
-    //       return (
-    //           <Nav.Link key={index}>
-    //               {item}
-    //           </Nav.Link>
-    //       )
-    //   })
-    // }
-    
 
     return (
       <>
-        <Navbar expand="lg" className="bg-body-tertiary">
+        <Navbar expand="lg" style={{ backgroundColor: 'white' }}>
           <Container fluid>
             {/* if login href = dashboard */}
             <Navbar.Brand href="/">Shift Builder</Navbar.Brand>

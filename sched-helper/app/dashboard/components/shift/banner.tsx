4fb56a5d12f2f3cdc51f567a7c121a5ecf14c21b
Eugene <lin.eugene.l.e@gmail.com>
2023-07-07 22:42:25 +0800

    Add icons to banner

--- sched-helper/app/dashboard/components/shift/banner.tsx
+++ sched-helper/app/dashboard/components/shift/banner.tsx
@@ -11,7 +11,12 @@ export default function Banner({shift_name} : {shift_name: string}){
                     <Navbar.Brand>
                         {shift_name}
                     </Navbar.Brand>
-                    <Col sm={9}>
+                    <Col sm={7}></Col>
+                    <Col>
+                        <Image className={styles.icon} src="/run.svg" width={30} height={30} alt="run"></Image>
+                    </Col>
+                    <Col>
+                        <Image className={styles.icon} src="/reset.svg" width={30} height={30} alt="reset"></Image>
                     </Col>
                     <Col>
                         <Image className={styles.icon} src="/info.svg" width={30} height={30} alt="Info"></Image>

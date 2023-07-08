c024f624571ffe539548c80f2f4a4481b5b29eaf
Eugene <lin.eugene.l.e@gmail.com>
2023-07-08 15:28:56 +0800

    Add rename api

---
+++ sched-helper/app/dashboard/api/shifts/rename/route.tsx
@@ -0,0 +1,34 @@
+import clientPromise from "@/lib/mongodb";
+import { NextResponse, NextRequest } from "next/server";
+
+import { z } from "zod";
+
+const schema = z.object({
+    name: z.string(),
+    shift_id: z.string(),
+})
+
+export async function POST(request: Request){
+    try{
+        const client = await clientPromise;
+        const db = await client.db();
+        const collection = await db.collection('shifts-list');
+
+
+        const json = await request.json();
+        const body = schema.parse(json);
+
+        const filter = {shift_id: body.shift_id};
+        const updateDoc = {
+            $set: {
+                shift_name: body.name,
+            },
+        };
+
+        const result = await collection.updateOne(filter, updateDoc);
+        return NextResponse.json({"message": "success"});
+    }catch(err){
+        console.log(err);
+        return NextResponse.error();
+    }
+}
